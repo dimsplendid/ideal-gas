ca4a7fabeb81fd3006b40226310740b359a9ff8f
dimsplendid <dimsplendid@gmail.com>
2026-04-12 15:39:45 +0800

    Add trajectory

--- main.c
+++ main.c
@@ -8,25 +8,26 @@
 
 // Constant
 // like resource in Bevy
-#define PARTICLE_NUM        200
+#define PARTICLE_NUM        50
 #define PARTICLE_RADIUS     30
-#define MAX_VELOCITY        200
+#define MAX_VELOCITY        400
 
 #define GRID_LEN            20
-#define CELL_NUM            GRID_LEN*GRID_LEN*GRID_LEN
-
+#define CELL_NUM            (GRID_LEN*GRID_LEN*GRID_LEN)
+#define BIN_COUNT           30
+#define TRAJECTORY_LEN      100
 // Layout
 #define PANEL_WIDTH         250
 #define WALL_WIDTH          800
 #define WALL_HEIGHT         600
 #define WALL_DEPTH          800
-#define WINDOW_WIDTH        PANEL_WIDTH+WALL_WIDTH
+#define WINDOW_WIDTH        (PANEL_WIDTH+WALL_WIDTH)
 #define WINDOW_HEIGHT       WALL_HEIGHT
 #define BACKGROUND          GetColor(0x181818FF)
 
 // render far particle
 #define GAMMA               1.8f
-#define RENDER_DEPTH_FACTOR 1.0f/WALL_DEPTH
+#define RENDER_DEPTH_FACTOR (1.0f/WALL_DEPTH)
 #define DIM_RATIO           0.7f
 
 // States
@@ -42,6 +43,12 @@ Vector3 vec_add(Vector3 a, Vector3 b) {
         .z = a.z + b.z,
     };
 }
+
+// meta data
+// -> contain which component
+// -> query? tags?
+// -> tag array [1, 0, 1, 0, 0, 0, ...]
+// trajectory -> entity or compnent...?
 
 Vector3 vec_sub(Vector3 a, Vector3 b) {
     return (Vector3) {
@@ -71,61 +78,86 @@ float vec_norm(Vector3 v) {
     return sqrtf(vec_dot(v, v));
 }
 
-// Entity
-typedef struct {
-    Vector3 min;
-    Vector3 max;
-    // float lineThick;
-} Box;
+void vec_print(Vector3 v) {
+    printf("v = (%f, %f, %f)", v.x, v.y, v.z);
+}
 
+// Entity
 struct {
     size_t *items;
     size_t count;
     size_t capacity;
 } entity = {0};// store id only
 
+typedef struct {
+    Vector3 min;
+    Vector3 max;
+    // float lineThick;
+} Box;
+
 // TODO: also add the box to entities and render
 Box *wall = &(Box) {
     .min = (Vector3) {
-        .x=-WALL_WIDTH /2,
-        .y=-WALL_HEIGHT/2,
-        .z=0             ,
+        .x=-(float)WALL_WIDTH /2,
+        .y=-(float)WALL_HEIGHT/2,
+        .z=0,
     },
     .max = (Vector3) {
-        .x= WALL_WIDTH /2,
-        .y= WALL_HEIGHT/2,
-        .z= WALL_DEPTH   ,
+        .x= (float)WALL_WIDTH /2,
+        .y= (float)WALL_HEIGHT/2,
+        .z= WALL_DEPTH,
     }
 };
-    
+
 // Component
-typedef struct {
-    Vector3 pos;
-    Vector3 vel;
-    Color color;
-} Particle;
-
-struct {
-    // index indicate an entity
-    // only particle for now, maybe using Union type sys later
-    // or lots of array to handle component?
-    Particle *items;
-    size_t count;
-    size_t capacity;
-} particles = {0};
-
-// try more ecs method
 // separate components, all tracked by ids
 struct {
-    Vector3 *items;
+    float *items;
     size_t count;
     size_t capacity;
-} velocity_component = {0};
+} Vx = {0};
 struct {
+    float *items;
+    size_t count;
+    size_t capacity;
+} Vy = {0};
+struct {
+    float *items;
+    size_t count;
+    size_t capacity;
+} Vz = {0};
+struct {
+    float *items;
+    size_t count;
+    size_t capacity;
+} Px = {0};
+struct {
+    float *items;
+    size_t count;
+    size_t capacity;
+} Py = {0};
+struct {
+    float *items;
+    size_t count;
+    size_t capacity;
+} Pz = {0};
+struct {
+    Color *items;
+    size_t count;
+    size_t capacity;
+} color = {0};
+
+typedef struct {
     Vector3 *items;
     size_t count;
     size_t capacity;
-} position_component = {0};
+} Trajectory;
+
+struct {
+    Trajectory *items;
+    size_t count;
+    size_t capacity;
+} trajectory = {0};
 
 // tracking render order
 struct {
@@ -194,69 +226,69 @@ void neighbor_cells_init() {
 
 void spawn_random_particles(size_t particle_numbers) {
     for (size_t i = 0; i < particle_numbers; ++i) {
-        Particle p = {0};
-        p.pos = (Vector3){
-            .x = GetRandomValue((PARTICLE_RADIUS+wall->min.x)*0.01f,(wall->max.x-PARTICLE_RADIUS)*0.01f),
-            .y = GetRandomValue((PARTICLE_RADIUS+wall->min.y)*0.01f,(wall->max.y-PARTICLE_RADIUS)*0.01f),
-            .z = GetRandomValue((PARTICLE_RADIUS+wall->min.z)*0.01f,(wall->max.z-PARTICLE_RADIUS)*0.01f),
-        };
-        p.vel = (Vector3) {
-            .x = GetRandomValue(-MAX_VELOCITY, MAX_VELOCITY),
-            .y = GetRandomValue(-MAX_VELOCITY, MAX_VELOCITY),
-            .z = GetRandomValue(-MAX_VELOCITY, MAX_VELOCITY),
-        };
-        p.color = (Color) {
+        da_append(&Px, (float)GetRandomValue((PARTICLE_RADIUS+wall->min.x)*0.1,(wall->max.x-PARTICLE_RADIUS)*0.1));
+        da_append(&Py, (float)GetRandomValue((PARTICLE_RADIUS+wall->min.y)*0.1,(wall->max.y-PARTICLE_RADIUS)*0.1));
+        da_append(&Pz, (float)GetRandomValue((PARTICLE_RADIUS+wall->min.z)*0.1,(wall->max.z-PARTICLE_RADIUS)*0.1) + 10.0f);
+        da_append(&Vx, 0);
+        da_append(&Vy, 0);
+        da_append(&Vz, (float) -MAX_VELOCITY);
+        da_append(&color,((Color){
             .r = GetRandomValue(50, 255),
             .g = GetRandomValue(50, 255),
             .b = GetRandomValue(50, 255),
             .a = 255,
-        };
-        da_append(&particles, p);
+        }));
         da_append(&render_order, i);
-        grid_put(i, &p.pos);
+        da_append(&entity, i);
+        grid_put(i, & (Vector3) {Px.items[i], Py.items[i], Pz.items[i]});
+
+        // Trajectory
+        da_append(&trajectory, ((Trajectory) {0}));
     }
-        
     neighbor_cells_init();
 }
 
 
 // System
 
-// perfestic_reset impact(after all, it's ideal gas...)
-void particle_collide(Particle *a, Particle *b) {
-    Vector3 delta       = vec_sub(b->pos, a->pos);
+// perfect elasstic impact(after all, it's ideal gas...)
+void particle_collide(size_t a, size_t b) {
+    Vector3 Pa = {Px.items[a], Py.items[a], Pz.items[a]};
+    Vector3 Pb = {Px.items[b], Py.items[b], Pz.items[b]};
+    Vector3 delta       = vec_sub(Pb, Pa);
     float   dist_sq     = vec_square_norm(delta);
     float   min_dist_sq = 4 * PARTICLE_RADIUS * PARTICLE_RADIUS;
-    
-    if (dist_sq < min_dist_sq) {
-        Vector3 v_rel = vec_sub(b->vel, a->vel); // relative velocity
-        float v_dot_delta = vec_dot(v_rel, delta);
-        // only calculate collide velocity when getting close
-        if (v_dot_delta < 0) {
-            float scalar = v_dot_delta / dist_sq;
-            Vector3 impulse = vec_scale(scalar, delta);
 
-            a->vel = vec_add(a->vel, impulse);
-            b->vel = vec_sub(b->vel, impulse);
-            // overlap modified
-            // > would cause weird juggling, cancel.
-            // float dist = sqrtf(dist_sq);
-            // float overlap = 2 * PARTICLE_RADIUS - dist;
-            // Vector3 separation = vec_scale(overlap * 0.5f / dist, delta);
-            // a->pos = vec_sub(a->pos, separation);
-            // b->pos = vec_add(b->pos, separation);
-        }
-    }
+    // only calculate velocity change when touching
+    if (dist_sq > min_dist_sq) return;
+    Vector3 Va = {Vx.items[a], Vy.items[a], Vz.items[a]};
+    Vector3 Vb = {Vx.items[b], Vy.items[b], Vz.items[b]};
+    Vector3 v_rel = vec_sub(Vb, Va); // relative velocity
+    float v_dot_delta = vec_dot(v_rel, delta);
+    
+    // only calculate collide velocity when getting close
+    if (v_dot_delta > 0) return;
+    float scalar = v_dot_delta / dist_sq;
+    Vector3 impulse = vec_scale(scalar, delta);
+
+    Va = vec_add(Va, impulse);
+    Vb = vec_sub(Vb, impulse);
+    Vx.items[a] = Va.x;
+    Vy.items[a] = Va.y;
+    Vz.items[a] = Va.z;
+    Vx.items[b] = Vb.x;
+    Vy.items[b] = Vb.y;
+    Vz.items[b] = Vb.z;
 }
 
 void particle_collide_sys() {
     TODO("");
 }
 
-void box_collide(Particle *p, Box *box) {
-    if ((p->pos.x-PARTICLE_RADIUS < box->min.x && p->vel.x < 0) || (p->pos.x+PARTICLE_RADIUS > box->max.x && p->vel.x > 0)) p->vel.x = -p->vel.x;
-    if ((p->pos.y-PARTICLE_RADIUS < box->min.y && p->vel.y < 0) || (p->pos.y+PARTICLE_RADIUS > box->max.y && p->vel.y > 0)) p->vel.y = -p->vel.y;
-    if ((p->pos.z-PARTICLE_RADIUS < box->min.z && p->vel.z < 0) || (p->pos.z+PARTICLE_RADIUS > box->max.z && p->vel.z > 0)) p->vel.z = -p->vel.z;
+void box_collide(size_t p, Box *box) {
+    if ((Px.items[p]-PARTICLE_RADIUS < box->min.x && Vx.items[p] < 0) || (Px.items[p]+PARTICLE_RADIUS > box->max.x && Vx.items[p] > 0)) Vx.items[p] = -Vx.items[p];
+    if ((Py.items[p]-PARTICLE_RADIUS < box->min.y && Vy.items[p] < 0) || (Py.items[p]+PARTICLE_RADIUS > box->max.y && Vy.items[p] > 0)) Vy.items[p] = -Vy.items[p];
+    if ((Pz.items[p]-PARTICLE_RADIUS < box->min.z && Vz.items[p] < 0) || (Pz.items[p]+PARTICLE_RADIUS > box->max.z && Vz.items[p] > 0)) Vz.items[p] = -Vz.items[p];
 }
 
 void box_collide_sys() {
@@ -278,12 +310,38 @@ typedef struct {
 
 Statistic statistic = {0};
 
+
+float vel_bin[BIN_COUNT]  = {0};
+float vel2_bin[BIN_COUNT] = {0};
+
 void analysis() {
     float v_square_sum = 0;
     float v_sum = 0;
-    da_foreach(Particle, p, &particles) {
-        v_square_sum += vec_square_norm(p->vel);
-        v_sum        += vec_norm(p->vel);
+    for (size_t i = 0; i < BIN_COUNT; ++i) {
+        vel_bin[i] = 0;
+        vel2_bin[i] = 0;
+    }
+    float v_max = 0;
+    float v2_max = 0;
+    for (size_t i = 0; i < entity.count; ++i) {
+        Vector3 vi    = {Vx.items[i], Vy.items[i], Vz.items[i]};
+        float v2n     = vec_square_norm(vi);
+        float vn      = sqrtf(v2n);
+        v_max = v_max > vn ? v_max : vn;
+        v2_max = v2_max > v2n ? v2_max : v2n;
+    }
+    for (size_t i = 0; i < entity.count; ++i) {
+        Vector3 vi    = {Vx.items[i], Vy.items[i], Vz.items[i]};
+        float v2n     = vec_square_norm(vi);
+        float vn      = sqrtf(v2n);
+        v_sum        += vn;
+        v_square_sum += v2n;
+        size_t bin = vn / (v_max / BIN_COUNT);
+        if (bin >= BIN_COUNT) bin = BIN_COUNT - 1;
+        vel_bin[bin] += 1.0f / PARTICLE_NUM;
+        bin = v2n / (v2_max / BIN_COUNT);
+        if (bin >= BIN_COUNT) bin = BIN_COUNT - 1;
+        vel2_bin[bin] += 1.0f / PARTICLE_NUM;
     }
     float v_square_avg = v_square_sum / PARTICLE_NUM;
     float v_avg        = v_sum        / PARTICLE_NUM;
@@ -293,7 +351,7 @@ void analysis() {
     statistic.kinetic_energy = v_square_avg;
 }
 
-// Math Plot Lib
+// TODO: Math Plot Lib
 typedef struct {
     Color color;
     float thick;
@@ -335,54 +393,122 @@ Color depth_color(Color origin, float z) {
 // sense, just cover on it :)
 // sorted by depth to mimic the depth cover effect
 int particle_depth_compare(const void *a, const void *b) {
-    const Particle p1 = particles.items[*(const size_t *)a];
-    const Particle p2 = particles.items[*(const size_t *)b];
-    if (p1.pos.z == p2.pos.z) return 0;
-    return p1.pos.z > p2.pos.z ? -1 : 1;
+    const size_t Pa_z = Pz.items[*(const size_t *)a];
+    const size_t Pb_z = Pz.items[*(const size_t *)b];
+    if (Pa_z == Pb_z) return 0;
+    return Pa_z > Pb_z ? -1 : 1;
 }
 
 void render_sort() {
     qsort(render_order.items, render_order.count, sizeof(void*), particle_depth_compare);
 }
 
+size_t count = 0;
+void trajectory_update() {
+    count++;
+    for (size_t i = 0; i < entity.count; ++i) {
+        Vector3 pos = {Px.items[i], Py.items[i], Pz.items[i]};
+        Trajectory *t = &trajectory.items[i];
+        if (count < TRAJECTORY_LEN) {
+            da_append(t, pos);
+        } else {
+            t->items[count % TRAJECTORY_LEN] = pos;
+        }
+        // // some free bugs using shift()
+        // if (t->count > TRAJECTORY_LEN) {
+        //     Vector3 *v = t->items;
+        //     t->items += 1;
+        //     t->count -= 1;
+        //     // free(v);
+        // };
+    }
+}
 
-void update() {
-    if (pause) return; // <- control by events
-    float dt = GetFrameTime();
-    if (dt > 0.1) return; // block when render block
+void velocity_update() {
+    // perfect elasstic impact
     for (size_t i = 0; i < CELL_NUM; ++i) {
         Cell * cell = &grid[i];
-        // TODO: neighbor_cells generate at the beginning
         CellId * neighbor_cell = &neighbor_cells[i];
         da_foreach(size_t, p1, cell) {
             size_t id1 = *p1;
-            Particle * particle1 = particles.items+id1;
             da_foreach(size_t, box_idx, neighbor_cell) {
                 Cell * sur_cell = &grid[*box_idx];
                 da_foreach(size_t, p2, sur_cell) {
                     size_t id2 = *p2;
                     if (id1 <= id2) continue;
-                    particle_collide(particle1, particles.items+id2);          
+                    particle_collide(id1, id2);          
                 }
             }
+            box_collide(id1, wall);
         }
-    }
-
-    // grid refill
-    grid_reset();
-    da_foreach(Particle, p, &particles) {
-        box_collide(p, wall);
-        p->pos = vec_add(p->pos,vec_scale(dt, p->vel));
-        grid_put(p-particles.items, &p->pos);
     }
 }
 
+void position_update(float dt) {
+    grid_reset();
+    for (size_t i = 0; i < entity.count; ++i) {
+        Px.items[i] = Px.items[i] + Vx.items[i] * dt;
+        Py.items[i] = Py.items[i] + Vy.items[i] * dt;
+        Pz.items[i] = Pz.items[i] + Vz.items[i] * dt;
+        // position grid refill
+        grid_put(i, & (Vector3) {Px.items[i], Py.items[i], Pz.items[i]});
+    }
+}
+
+void update() {
+    if (pause) return; // <- control by events
+    float dt = GetFrameTime();
+    if (dt > 0.1) return; // block when render block
+
+    velocity_update();
+    position_update(dt);
+    trajectory_update();
+}
+
 // particle project to screen
-Vector2 screen(Particle *p) {
+Vector2 screen(Vector3 pos) {
     return (Vector2) {
-        .x = p->pos.x / (p->pos.z*RENDER_DEPTH_FACTOR + 1) + WALL_WIDTH /2 + PANEL_WIDTH,
-        .y = p->pos.y / (p->pos.z*RENDER_DEPTH_FACTOR + 1) + WALL_HEIGHT/2,
+        .x = pos.x / (pos.z*RENDER_DEPTH_FACTOR + 1) + (float)WALL_WIDTH /2 + PANEL_WIDTH,
+        .y = pos.y / (pos.z*RENDER_DEPTH_FACTOR + 1) + (float)WALL_HEIGHT/2,
     };
+}
+
+void draw_circle_fake3d (Vector3 center, float radius, Color color) {
+    // printf("start draw circle 3d fake\n");
+    DrawCircleV(
+        screen(center),
+        radius/(center.z*RENDER_DEPTH_FACTOR + 1),
+        depth_color(color, center.z)
+    );
+}
+
+void trajectory_render() {
+    for (size_t i = 0; i < trajectory.count; ++i) {
+        Trajectory t = trajectory.items[i];
+        da_foreach(Vector3, p, &t) {
+            draw_circle_fake3d(*p, 2, color.items[i]);
+        }
+    }
+}
+
+void bin_render(
+    float bin[],
+    Vector2 position, // bottom-left
+    int width,
+    int height
+) {
+    // int spacing = width / 40;
+    int spacing = 2;
+    int bin_width = (width - spacing * (BIN_COUNT - 1)) / BIN_COUNT;
+    for (size_t i = 0; i < BIN_COUNT; ++i) {
+        DrawRectangle(
+            position.x + i * (spacing + bin_width),
+            position.y - height*bin[i],
+            bin_width,
+            height*bin[i],
+            WHITE
+        );
+    }
 }
 
 void render() {
@@ -391,17 +517,17 @@ void render() {
     render_sort();
     ClearBackground(BACKGROUND);
     da_foreach(size_t, index, &render_order) {
-        Particle *p = &particles.items[*index];
-        DrawCircleV(
-            screen(p),
-            PARTICLE_RADIUS/(p->pos.z*RENDER_DEPTH_FACTOR + 1),
-            depth_color(p->color, p->pos.z)
-        );
+        size_t i = *index;
+        Vector3 pos = {Px.items[i], Py.items[i], Pz.items[i]};
+        draw_circle_fake3d(pos, PARTICLE_RADIUS, color.items[i]);
     }
     // Panel
     DrawText("Ideal Gas Simulator", 20, 20, 20, WHITE);
     DrawText(TextFormat("<V2> = %.2f", statistic.v_square_avg), 20, 50, 20, WHITE);
     DrawText(TextFormat("<V>  = %.2f", statistic.v_avg       ), 20, 80, 20, WHITE);
+    bin_render( vel_bin, (Vector2) {20, 150}, PANEL_WIDTH, 300);
+    bin_render(vel2_bin, (Vector2) {20, 240}, PANEL_WIDTH, 300);
+    trajectory_render();
 }
 
 int main(void) {
@@ -420,18 +546,31 @@ int main(void) {
     //     printf(")\n");
     // }
     // printf("%d\n", CELL_NUM);
+    size_t frame = 0;
     while(!WindowShouldClose()) {
         BeginDrawing();
-        // TODO: maybe add to event() ?
+        // TODO: maybe add to event() later?
         if (IsKeyPressed(KEY_P)) pause = !pause;
         update();
-        analysis();
+        if (frame % 60 == 0) analysis();
         render();
         EndDrawing();
     }
     CloseWindow();
+    // for (size_t i = 0; i < BIN_COUNT; ++i) {
+    //     printf("V[%zu]: %zu\n", i, vel_bin[i]);
+    // }
+    // for (size_t i = 0; i < BIN_COUNT; ++i) {
+    //     printf("V2[%zu]: %zu\n", i, vel2_bin[i]);
+    // }
     
-    da_free(particles);
+    da_free(Px);
+    da_free(Py);
+    da_free(Pz);
+    da_free(Vx);
+    da_free(Vy);
+    da_free(Vz);
+    da_free(color);
     da_free(render_order);
     return 0;
 }
